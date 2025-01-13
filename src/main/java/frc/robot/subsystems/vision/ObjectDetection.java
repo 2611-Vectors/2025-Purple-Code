a9288d9b64c7187437a9aeb849bf0a90ddbd4b52
RyanT-code <ryanlee83008@gmail.com>
2025-01-13 16:47:29 -0500

    Started vision code

---
+++ src/main/java/frc/robot/subsystems/vision/ObjectDetection.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.List;
+import org.littletonrobotics.junction.Logger;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class ObjectDetection extends SubsystemBase {
+  PhotonCamera camera = new PhotonCamera("Front Camera");
+  /** Creates a new ObjectDetection. */
+  public ObjectDetection() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+    // Getting target results from the camera
+    List<PhotonPipelineResult> results = camera.getAllUnreadResults();
+    // Looping throught the targets of the camera
+    for (int i = 0; i <= results.size(); i++) {
+      PhotonPipelineResult result = results.get(i);
+      // This is an list of all the properties of the i target
+      List<PhotonTrackedTarget> targets = result.targets;
+      // Looping to get the properties of the tracked target
+      for (int p = 0; p <= targets.size(); p++) {
+        PhotonTrackedTarget tracked = targets.get(p);
+        // Getting tracked target properties
+        double area = tracked.area;
+        double yaw = tracked.yaw;
+        double pitch = tracked.pitch;
+        // Logging target properties
+        Logger.recordOutput("Vision/ObjectsDetected/Area", area);
+        Logger.recordOutput("Vision/ObjectsDetected/Yaw", yaw);
+        Logger.recordOutput("Vision/ObjectsDetected/Pitch", pitch);
+      }
+    }
+  }
+}
+/*
+
+
+
+
+*/
