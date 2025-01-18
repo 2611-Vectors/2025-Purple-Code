6316df85c5efc15d9de3b9f9120c5215acc37f13
Arno Dorian <lee.cedric.2009@gmail.com>
2025-01-18 16:24:46 -0500

    Started work on an path builder custom thing

--- src/main/java/frc/robot/util/CustomAutoBuilder.java
+++ src/main/java/frc/robot/util/CustomAutoBuilder.java
@@ -16,8 +16,16 @@ public class CustomAutoBuilder {
   private static final Pose2d MIDDLE_START = new Pose2d(8.5, 1.9, START_ROTATION);
   private static final Pose2d LEFT_START = new Pose2d(8.5, 0.8, START_ROTATION);
 
-  public static LoggedDashboardChooser<Pose2d> startChooser;
+  private static final Pose2d BACK_RIGHT_SCORE = new Pose2d(5.2, 2.7, new Rotation2d(Math.toRadians(120)));
+  private static final Pose2d BACK_LEFT_SCORE = new Pose2d(3.7, 2.7, new Rotation2d(Math.toRadians(60)));
+  private static final Pose2d LEFT_SCORE = new Pose2d(3.0, 4.0, new Rotation2d(Math.toRadians(0)));
+  private static final Pose2d TOP_LEFT_SCORE = new Pose2d(3.8, 5.3, new Rotation2d(Math.toRadians(-60)));
+  private static final Pose2d TOP_RIGHT_SCORE = new Pose2d(5.3, 5.3, new Rotation2d(Math.toRadians(-120)));
+  private static final Pose2d RIGHT_SCORE = new Pose2d(6.0, 4.0, new Rotation2d(Math.toRadians(180)));
 
+
+
+  public static LoggedDashboardChooser<Pose2d> startChooser;
   public static LoggedDashboardChooser<Pose2d> scoreOneChooser;
 
   public static void chooserBuilder() {
@@ -28,5 +36,12 @@ public class CustomAutoBuilder {
     startChooser.addOption("Left", LEFT_START);
 
     scoreOneChooser = new LoggedDashboardChooser<Pose2d>("Score 1 Position");
+
+    scoreOneChooser.addOption("Back Right", BACK_RIGHT_SCORE);
+    scoreOneChooser.addOption("Back Left", BACK_LEFT_SCORE);
+    scoreOneChooser.addOption("Left", LEFT_SCORE);
+    scoreOneChooser.addOption("Top Left", TOP_LEFT_SCORE);
+    scoreOneChooser.addOption("Top Right", TOP_RIGHT_SCORE);
+    scoreOneChooser.addOption("Right", RIGHT_SCORE);
   }
 }
